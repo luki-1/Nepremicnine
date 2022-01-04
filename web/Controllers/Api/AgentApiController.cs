3c20c3e7d7a0456420b53f1aaba0ac7573f3ade5
Luki <37488014+luki-1@users.noreply.github.com>
2022-01-04 01:13:48 +0100

    Update AgentApiController.cs

--- web/Controllers/Api/AgentApiController.cs
+++ web/Controllers/Api/AgentApiController.cs
@@ -2,45 +2,107 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using Microsoft.EntityFrameworkCore;
+using web.Data;
+using web.Models;
 
 namespace web.Controllers.Api
 {
-    [Route("api/agent")]
-    public class Agent : Controller
+    [Route("api/v1/agent")]
+    [ApiController]
+    public class StudentsApiController : ControllerBase
     {
-        // GET: api/<controller>
+        private readonly NepremicnineContext _context;
+
+        public StudentsApiController(NepremicnineContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/StudentsApi
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<ActionResult<IEnumerable<Agent>>> GetStudents()
         {
-            return new string[] { "value1", "value2" };
+            return await _context.Agenti.ToListAsync();
         }
 
-        // GET api/<controller>/5
+        // GET: api/StudentsApi/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<ActionResult<Agent>> GetStudent(int id)
         {
-            return "value";
+            var student = await _context.Agenti.FindAsync(id);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return student;
         }
 
-        // POST api/<controller>
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/<controller>/5
+        // PUT: api/StudentsApi/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public async Task<IActionResult> PutStudent(int id, Agent student)
         {
+            if (id != student.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(student).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StudentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
         }
 
-        // DELETE api/<controller>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        // POST: api/StudentsApi
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Agent>> PostStudent(Agent student)
         {
+            _context.Agenti.Add(student);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetStudent", new { id = student.ID }, student);
+        }
+
+        // DELETE: api/StudentsApi/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteStudent(int id)
+        {
+            var student = await _context.Agenti.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            _context.Agenti.Remove(student);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool StudentExists(int id)
+        {
+            return _context.Agenti.Any(e => e.ID == id);
         }
     }
 }
